d8e82d3cab64d24f21b742c55e8c6af244be1c09
cisterni <cisterni@di.unipi.it>
2016-01-09 16:28:48 +0100

    Ups, forgot

---
+++ AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace SuaveEvReact.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Suave.EvReact")>]
+[<assembly: AssemblyDescription("This package turns URL accesses matched by Suave into EvReact events.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("IT Center, University of Pisa")>]
+[<assembly: AssemblyProduct("Suave.EvReact")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("f69e3e5d-d6f7-47cd-add7-ff24e03b1837")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do
+    ()
