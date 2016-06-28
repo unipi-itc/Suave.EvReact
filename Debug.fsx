cd2835fae2c982a09e3155a1b972575ee18f7bf6
Antonio Cisternino <cisterni@di.unipi.it>
2016-06-28 14:32:58 +0200

    Simple script for debugging purposes.

---
+++ Debug.fsx
@@ -0,0 +1,18 @@
+﻿
+#I __SOURCE_DIRECTORY__
+
+#r @"packages/Suave.1.1.3/lib/net40/Suave.dll"
+#r @"packages/Newtonsoft.Json.9.0.1/lib/net40/Newtonsoft.Json.dll"
+
+#load "Suave.EvReact.fs"
+open Suave.EvReact
+
+open Suave
+open Suave.Http
+open Suave.Successful
+open Suave.Web
+open Suave.Operators
+
+let wp, ev = createRemoteIEvent<int*int>()
+// Starts Suave
+startWebServer defaultConfig (choose [ Filters.path "/poster" >=> wp; Files.browse __SOURCE_DIRECTORY__ ])
